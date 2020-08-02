77f0d1f67bf7527573df544ad868f2601e13f58e
jpic <jpic@yourlabs.org>
2020-08-02 06:50:33 +0200

    Run(root)

--- shlax/actions/run.py
+++ shlax/actions/run.py
@@ -1,11 +1,15 @@
 
 
 class Run:
-    def __init__(self, cmd):
+    def __init__(self, cmd, root=False):
         self.cmd = cmd
+        self.root = root
 
     async def __call__(self, target):
-        self.proc = await target.exec(self.cmd)
+        if self.root:
+            self.proc = await target.rexec(self.cmd)
+        else:
+            self.proc = await target.exec(self.cmd)
 
     def __str__(self):
         return f'Run({self.cmd})'
